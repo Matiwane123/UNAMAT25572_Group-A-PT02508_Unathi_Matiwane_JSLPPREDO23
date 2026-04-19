b3297f328e462059f6d1b3813f736eeb6893e7ff
unathi Matiwane <110603176+Matiwane123@users.noreply.github.com>
2026-04-19 16:33:19 +0200

    pushing to github

---
+++ scripts/localStorage.js
@@ -0,0 +1,23 @@
+const initialTasks = [];
+
+export function loadTasksFromStorage() {
+  const stored = localStorage.getItem("tasks");
+
+  if (stored) {
+    try {
+      return JSON.parse(stored);
+    } catch (error) {
+      console.error("Error parsing tasks from localStorage:", error);
+
+      localStorage.setItem("tasks", JSON.stringify(initialTasks));
+      return initialTasks;
+    }
+  }
+
+  localStorage.setItem("tasks", JSON.stringify(initialTasks));
+  return initialTasks;
+}
+
+export function saveTasksToStorage(tasks) {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
